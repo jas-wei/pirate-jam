849abee373933fd705b75f205c135cf56d825d4a
Jasmine Wei <144985291+jas-wei@users.noreply.github.com>
2025-01-04 22:41:39 -0800

    healthbar
    made a health bar that reacts to the players health

--- pirate-jam/scripts/player.gd
+++ pirate-jam/scripts/player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 
+signal healthChanged
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var maxHealth = 30
+@onready var currentHealth = 14
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	healthChanged.emit()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,10 +21,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if (Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("up")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
+
+	#movements
 	if Input.is_action_pressed("left"):
 		animated_sprite_2d.flip_h = true
 		animated_sprite_2d.play("run")
