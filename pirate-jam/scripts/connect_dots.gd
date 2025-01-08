b368966462042d1041bfbaae200c076c44a51314
Jasmine Wei <144985291+jas-wei@users.noreply.github.com>
2025-01-08 15:41:12 -0800

    updated pause menu
    added sigil drawing to pause menu

--- pirate-jam/scripts/connect_dots.gd
+++ pirate-jam/scripts/connect_dots.gd
@@ -1,14 +1,14 @@
-extends Node2D
-
-
-@export var dots = []
-
+extends Control
 
 @onready var line_layer: Node = $line_layer
 @onready var vertices = []  # Array of all Area2D nodes
+@onready var button
+@onready var dots = []
 
 var current_line: Line2D = null  # Reference to the Line2D being drawn
 var start_vertex: Area2D = null  # The starting Area2D
+var area_entered = false
+@onready var area_2d: Area2D = $Area2D
 
 const air_sigil = [["dot2", "dot7"], ["dot2", "dot9"], ["dot4", "dot6"], ["dot7", "dot9"]]
 const fire_sigil = [["dot2", "dot7"], ["dot2", "dot9"], ["dot7", "dot9"]]
@@ -18,46 +18,49 @@ const earth_sigil = [["dot1", "dot3"], ["dot1", "dot8"], ["dot3", "dot8"], ["dot
 func _ready() -> void:
 	# Populate the vertices array with all Area2D children
 	vertices = get_tree().get_nodes_in_group("vertices")
+	button = get_tree().get_nodes_in_group("Button_container")[0].get_child(1)
+	print (button)
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
-		if compare_edges(dots, air_sigil):
-			print("air sigil drawn")
-		if compare_edges(dots, fire_sigil):
-			print("fire sigil drawn")
-		if compare_edges(dots, water_sigil):
-			print("water sigil drawn")
-		if compare_edges(dots, earth_sigil):
-			print("earth sigil drawn")
-	
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			# Find the nearest Area2D to the mouse position
-			start_vertex = get_nearest_vertex(get_global_mouse_position())
-			if start_vertex:
-				# Create a new Line2D and set its starting point
-				current_line = Line2D.new()
-				current_line.add_point(start_vertex.global_position)
-				current_line.add_point(get_global_mouse_position())  # Temporary endpoint
-				line_layer.add_child(current_line)
+	if button.disabled == true:
+		if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
+			if compare_edges(dots, air_sigil):
+				print("air sigil drawn")
+			if compare_edges(dots, fire_sigil):
+				print("fire sigil drawn")
+			if compare_edges(dots, water_sigil):
+				print("water sigil drawn")
+			if compare_edges(dots, earth_sigil):
+				print("earth sigil drawn")
 		
-		elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed and current_line:
-			# Find the nearest Area2D to the release position
-			var end_vertex = get_nearest_vertex(get_global_mouse_position())
-			#print("end", end_vertex)
-			if end_vertex and end_vertex != start_vertex:
-				# Snap the line to the endpoint and finalize
-				current_line.set_point_position(1, end_vertex.global_position)
-				print("Connected:", start_vertex.name, "to", end_vertex.name)
-				dots.append([str(start_vertex.name), str(end_vertex.name)])
-				print(dots)
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				# Find the nearest Area2D to the mouse position
+				start_vertex = get_nearest_vertex(get_global_mouse_position())
+				if start_vertex:
+					# Create a new Line2D and set its starting point
+					current_line = Line2D.new()
+					current_line.add_point(start_vertex.global_position)
+					current_line.add_point(get_global_mouse_position())  # Temporary endpoint
+					line_layer.add_child(current_line)
+			
+			elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed and current_line:
+				# Find the nearest Area2D to the release position
+				var end_vertex = get_nearest_vertex(get_global_mouse_position())
+				#print("end", end_vertex)
+				if end_vertex and end_vertex != start_vertex:
+					# Snap the line to the endpoint and finalize
+					current_line.set_point_position(1, end_vertex.global_position)
+					print("Connected:", start_vertex.name, "to", end_vertex.name)
+					dots.append([str(start_vertex.name), str(end_vertex.name)])
+					print(dots)
 
-			else:
-				# If no valid end vertex, remove the line
-				current_line.queue_free()
-			current_line = null
-			start_vertex = null
+				else:
+					# If no valid end vertex, remove the line
+					current_line.queue_free()
+				current_line = null
+				start_vertex = null
 
 func _process(delta: float) -> void:
 	if current_line:
@@ -88,3 +91,12 @@ func compare_edges(dots: Array, answer_key: Array) -> bool:
 	#print(dots)
 
 	return dots == answer_key
+
+
+#func _on_area_2d_mouse_entered() -> void:
+	#print("entered")
+	#area_entered = true
+#
+#
+#func _on_area_2d_mouse_exited() -> void:
+	#area_entered = false
--- pirate-jam/scripts/player/GUI/GUI.gd
+++ pirate-jam/scripts/player/GUI/GUI.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+
+
 func _ready() -> void:
 	get_node("Container").hide()
 	
@@ -13,19 +15,25 @@ func _on_inventory_pressed() -> void:
 	hideGUI()
 	$Container/Panel/VBoxContainer2/Inventory.disabled = true
 	$Container/Inventory.show()
+	var children = $Container/connect_dots/line_layer.get_children()
+	for child in children:
+		child.hide()
 
-
-
-func _on_achievements_pressed() -> void:
+func _on_spells_pressed() -> void:
 	hideGUI()
-
+	$Container/Panel/VBoxContainer2/Spells.disabled = true
+	$Container/connect_dots.show()
+	var children = $Container/connect_dots/line_layer.get_children()
+	for child in children:
+		child.show()
 
 func _on_main_menu_pressed() -> void:
 	hideGUI()
 
 func hideGUI():
-	$Container/Panel/VBoxContainer2/Achievements.disabled = false
+	$Container/Panel/VBoxContainer2/Spells.disabled = false
 	$Container/Panel/VBoxContainer2/Inventory.disabled = false
 	$Container/Panel/VBoxContainer2/Main_Menu.disabled = false
 	$Container/Inventory.hide()
+	$Container/connect_dots.hide()
 	
