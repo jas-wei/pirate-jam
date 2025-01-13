4d5379362448783a5d5d3d046443cc59c3a9b678
harshsreelal <harshsreelal@hotmail.com>
2025-01-12 21:49:53 -0800

    hurtbox/hitbox changes

--- pirate-jam/scripts/hurtbox.gd
+++ pirate-jam/scripts/hurtbox.gd
@@ -1,16 +1,25 @@
-class_name MyHurtBox
+class_name HurtBox
 extends Area2D
+
+signal recieved_damage(damage: int)
+
+#@export var health: Health
+
+
+
 
 func _init() -> void:
 	collision_layer = 0
 	collision_mask = 2
 	 
 func _ready() -> void:
-	connect("area_entered", self._on_area_entered)
+	connect("area_entered", _on_area_entered)
 	
 func _on_area_entered(hitbox: HitBox) -> void:
 	if hitbox == null:
-		return
+		print("Damage to enemy")
+		recieved_damage.emit(hitbox.damage)
+		
 
-	if owner.has_method("take_damage"):
-		owner.take_damage(hitbox.damage)
+	#if owner.has_method("take_damage"):
+		#owner.take_damage(hitbox.damage)
--- pirate-jam/scripts/player.gd
+++ pirate-jam/scripts/player.gd
@@ -6,12 +6,22 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @export var maxHealth = 30
-@onready var currentHealth = 0
+@export var attacking = false
 
+@onready var currentHealth = 0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation = $AnimationPlayer
 
 func _ready():
 	healthChanged.emit()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack"):
+		attack()
+	
+func attack():
+	attacking = true
+	animation.play("attack1")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -30,16 +40,11 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("right"):
 		animated_sprite_2d.flip_h = false
 		animated_sprite_2d.play("run")
-		velocity.x = SPEED
-	#supposed to attack but dunno
-	#elif Input.is_action_pressed("attack"):
-	#	$AnimationPlayer.play("attack1")	
+		velocity.x = SPEED	
 	else:
 		animated_sprite_2d.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if !is_on_floor():
 		animated_sprite_2d.play("jump")
 		
-	
-
 	move_and_slide()
