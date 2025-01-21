080ca97efe61023060e9c2c1c838be03299d8429
aleksial <125409114+aleksial@users.noreply.github.com>
2025-01-20 21:12:20 -0800

    fixed main menu and added volume page that doesnt do anything

--- pirate-jam/scripts/main_menu.gd
+++ pirate-jam/scripts/main_menu.gd
@@ -3,6 +3,11 @@ extends Control
 func _on_play_pressed() -> void: #this works but camera is wrong
 	get_tree().change_scene_to_file("res://scenes/level1.1.tscn")
 
-
-func _on_options_pressed() -> void: #haven't made this yet
+func _on_build_your_homo_pressed() -> void:
 	pass # Replace with function body.
+	
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit() # Replace with function body.
