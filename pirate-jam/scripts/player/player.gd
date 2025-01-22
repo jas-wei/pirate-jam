4eb74414fe122accdcbb76d1d87c20f38004c74a
harshsreelal <harshsreelal@hotmail.com>
2025-01-21 22:12:12 -0800

    changed player script

--- pirate-jam/scripts/player/player.gd
+++ pirate-jam/scripts/player/player.gd
@@ -6,7 +6,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @export var maxHealth = 30
-@onready var currentHealth = 10
+@onready var currentHealth = 0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -19,7 +19,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if (Input.is_action_pressed("jump") or Input.is_action_pressed("up")) and is_on_floor():
+	if (Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("up")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	#movements
