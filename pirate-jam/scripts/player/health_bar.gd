6f8de71e7132316b8988b0ac8fcf09ed169da313
Jasmine Wei <144985291+jas-wei@users.noreply.github.com>
2025-01-22 23:41:10 -0800

    reorgnaized files

--- pirate-jam/scripts/player/health_bar.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-@onready var player: CharacterBody2D = $"../player"
-
-func _ready():
-	player.healthChanged.connect(update)
-	update()
-
-func update():
-	$TextureProgressBar.value = player.currentHealth  * 100 / player.maxHealth
--- pirate-jam/scripts/signal_bus.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-signal on_health_changed(node: Node, amount_changed: int)
